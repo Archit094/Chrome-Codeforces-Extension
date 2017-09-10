634eb7e6adffd825e25cad78e57a8968422d079e
Sai Avinash <saiavin@gmail.com>
2017-09-10 17:06:30 +0530

    Indentation correction

--- popup.js
+++ popup.js
@@ -1,14 +1,10 @@
-/**
- * Created by archit on 27/07/17.
- */
 refresh() ;
 function refresh()
 {
     console.log('method called') ;
     var items = getUsers() ;
     console.log(items) ;
-    for(var ind in items)
-    {
+    for(var ind in items){
         $('#friends_list').append('<li><a >'+items[ind]+'</a></li>') ;
     }
 }
